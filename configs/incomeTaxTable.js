8111b4531c2384c67d257c8a3039fe59b79e5dfe
Francis Oscar Magtuto <francis03@simplexi.com.ph>
2020-01-14 15:42:27 +0800

    add table for incometax

---
+++ configs/incomeTaxTable.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        excessOver: 250000,
+        additional: 0,
+        percentage: 0.2
+    },
+    {
+        excessOver: 400000,
+        additional: 30000,
+        percentage: 0.25
+    },
+    {
+        excessOver: 800000,
+        additional: 130000,
+        percentage: 0.3
+    },
+    {
+        excessOver: 2000000,
+        additional: 490000,
+        percentage: 0.32
+    },
+    {
+        excessOver: 8000000,
+        additional: 2410000,
+        percentage: 0.35
+    }
+]
