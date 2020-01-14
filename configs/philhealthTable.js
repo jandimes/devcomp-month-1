edaa0ca23902ce2cf05723d5bfe847f241d89882
Francis Oscar Magtuto <francis03@simplexi.com.ph>
2020-01-14 12:34:10 +0800

    added philhealthTable.js

---
+++ configs/philhealthTable.js
@@ -0,0 +1,26 @@
+module.exports = {
+   2019 : {
+        rate: 0.0275,
+        maxSalary: 50000 
+   },
+   2020 : {
+        rate: 0.03,
+        maxSalary: 60000
+   },
+   2021 : {
+        rate: 0.035,
+        maxSalary: 70000
+   },
+   2022 : {
+        rate: 0.04,
+        maxSalary: 80000
+   },
+   2023 : {
+        rate: 0.045,
+        maxSalary: 90000
+   },
+   2024 : {
+        rate: 0.05,
+        maxSalary: 100000
+    },
+}
