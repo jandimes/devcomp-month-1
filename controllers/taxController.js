283cdff9f8ff12c2db6fcbf46771badcca17625a
Jan Caleb Salvador Manzano <jan01@simplexi.com.ph>
2020-01-14 10:14:57 +0800

    Created function for each computation

--- controllers/taxController.js
+++ controllers/taxController.js
@@ -2,18 +2,53 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    let input = {
+    const input = {
         monthlySalary: req.query.monthlySalary,
         year: req.query.year
     };
 
+    const data = {
+        monthlyWithholdingTax: computeMonthlyWithholdingTax(input),
+        totalYearlyIncomeTax: computeTotalYearlyIncomeTax(input),
+        sss: computeSSS(input),
+        philhealth: computePhilhealth(input),
+        pagibig: computePagibig(input),
+        thirteenthMonthPayTax: computeThirteenthMonthPayTax(input)
+    };
+
     const response = {
         success: true,
-        data: input,
+        data: data,
         message: "Tax computed. "
     };
 
     return res.json(response);
 });
+
+
+
+function computeMonthlyWithholdingTax(input) {
+    return 0;
+}
+
+function computeTotalYearlyIncomeTax(input) {
+    return 0;
+}
+
+function computeSSS(input) {
+    return 0;
+}
+
+function computePhilhealth(input) {
+    return 0;
+}
+
+function computePagibig(input) {
+    return 0;
+}
+
+function computeThirteenthMonthPayTax(input) {
+    return 0;
+}
 
 module.exports = router;
